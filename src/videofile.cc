70cac47410a89bb5bca9f48b41d5fdc1ee86cb1f
Haydn King <hjking734@gmail.com>
2015-08-10 19:49:30 +0100

    Open and close builds

--- src/videofile.cc
+++ src/videofile.cc
@@ -13,16 +13,18 @@ VideoFile::~VideoFile(){
 }
 
 bool VideoFile::open(const char* filename){
-    AVCodecContext *tempCodecContext;
+    AVCodecContext *tempCodecCtx;
 
     //close anything that's already open
     close();
 
-    if(avformat_open_input(formatCtx, filename, NULL, 0, NULL)!=0){
+    if(avformat_open_input(&formatCtx, filename, NULL, NULL)!=0){
+        close();
         return false; //failed to open file
     }
 
     if(avformat_find_stream_info(formatCtx, NULL)<0){
+        close();
         return false; // Couldn't find stream information
     }
 
@@ -30,13 +32,14 @@ bool VideoFile::open(const char* filename){
 
     // Find the first video stream
     videoStream=-1;
-    for(int i=0; i<pFormatCtx->nb_streams; i++)
-      if(formatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
-        videoStream=i;
-        break;
-      }
+    for(int i=0; i<formatCtx->nb_streams; i++)
+        if(formatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
+            videoStream=i;
+            break;
+        }
     if(videoStream==-1){
-      return false; // Didn't find a video stream
+        close();
+        return false; // Didn't find a video stream
     }
 
     // Get a pointer to the codec context for the video stream
@@ -45,21 +48,39 @@ bool VideoFile::open(const char* filename){
     // Find the decoder for the video stream
     codec = avcodec_find_decoder(tempCodecCtx->codec_id);
     if(codec==NULL) {
+        close();
         return false;
     }
     // Copy context
     codecCtx = avcodec_alloc_context3(codec);
-    if(avcodec_copy_context(codecCtx, tempCodecContext) != 0) {
-      return false; // Error copying codec context
+    if(avcodec_copy_context(codecCtx, tempCodecCtx) != 0) {
+        close();
+        return false; // Error copying codec context
     }
     // Open codec
-    if(avcodec_open2(codecCtx, codec)<0){
-      return false; // Could not open codec
+    if(avcodec_open2(codecCtx, codec, NULL)<0){
+        close();
+        return false; // Could not open codec
     }
+
+    return true;
 }
 
 bool VideoFile::isOpen(){
+    return (codecCtx != NULL) && (formatCtx != NULL);
+}
 
+void VideoFile::close(){
+
+    if(codecCtx != NULL){
+        avcodec_close(codecCtx);
+        codecCtx = NULL;
+    }
+    codec = NULL;
+    if(formatCtx != NULL){
+        avformat_close_input(&formatCtx);
+        formatCtx = NULL;
+    }
 }
 
 
