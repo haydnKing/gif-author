08a11401c9e5e8c63c7f205aedff55cbde2e3c47
Haydn King <hjking734@gmail.com>
2015-08-23 15:04:17 +0100

    Improve layout

--- src/videoplayer.cc
+++ src/videoplayer.cc
@@ -3,11 +3,14 @@
 #include <iostream>
 
 VideoPlayer::VideoPlayer(){
-    frame = 0;
+    frame = NULL;
+
+    set_row_spacing(10);
 
     w_image.set_vexpand(true);
     w_image.set_hexpand(true);
     attach(w_image, 0,0,1,1);
+    w_control.set_halign(Gtk::ALIGN_CENTER);
     attach(w_control, 0,1,1,1);
 
     w_control.signal_frame_next().connect(sigc::mem_fun(*this, &VideoPlayer::on_frame_next));
