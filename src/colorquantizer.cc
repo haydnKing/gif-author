0984818337f33286f41837466856119ac49f6956
Haydn King <hjking734@gmail.com>
2016-02-10 17:44:23 +0000

    bit conversion stuff and histogram

--- src/colorquantizer.cc
+++ src/colorquantizer.cc
@@ -12,35 +12,49 @@ class MMCQuantizer : public ColorQuantizer
         GIFColorTable get_ct();
 
     protected:
+        unsigned int color_to_index(const uint8_t* color) const;
+
         uint32_t *hist;
 };
 
-MMCQuantizerMMCQuantizer()
+MMCQuantizer::MMCQuantizer()
 {
     //2^17, number of quantum spaces
     hist = new uint32_t[131072];
+    std::memset(hist, 0, 131072*sizeof(uint32_t));
 };
 
-virtual MMCQuantizer~MMCQuantizer()
+virtual MMCQuantizer::~MMCQuantizer()
 {
     delete [] hist;
 };
 
 void MMCQuantizer::add_color(uint8_t* color)
 {
+    hist[color_to_index(color)]++;
 }
 
-void MMCQuantizer::build_ct(std::vector<uint8_t*> colors)
+void MMCQuantizer::build_ct()
 {
 };
 
 int MMCQuantizer::map_to_ct(uint8_t* color) const
 {
+    return hist[color_to_index(color)];
 };
 
 GIFColorTable MMCQuantizer::get_ct()
 {
 };
+        
+unsigned int MMCQuantizer::color_to_index(const uint8_t* color) const
+{
+    //6 bits red, 6 bits green, 5 bits blue
+    unsigned int r = ((color[0] & 252) << 9) + 
+                     ((color[1] & 252) << 3) + 
+                     (color[2] >> 3);
+    return r;
+}
 
 pColorQuantizer ColorQuantizer::get_quantizer(QuantizerMethod m)
 {
