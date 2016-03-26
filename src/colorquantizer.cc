dbdd7a46d91317799fcf15cd3bc18250e3c567aa
Haydn King <hjking734@gmail.com>
2016-03-26 11:54:49 +0000

    Simplify and rewrite split code

--- src/colorquantizer.cc
+++ src/colorquantizer.cc
@@ -89,10 +89,9 @@ class MMCQuantizer : public ColorQuantizer
 
             private:
                 void swap_px(const int& a, const int& b);
-                //int partition(int start, int end, int pivot, int channel);
-                int partition_by_value(int start, int end, float value, int channel);
-                int partition_channel(int channel);
-                void set_front_back(int start, int end, int channel);
+                int partition(int start, int end, float value, int channel,
+                              uint8_t& left_high, uint8_t& right_low);
+                uint8_t get_split_value(int channel);
 
                 vbox *left, *right;
                 int split_channel, ct_index;
@@ -262,183 +261,128 @@ void MMCQuantizer::vbox::swap_px(const int& a, const int& b)
         px[3*a+i] = t;
     }
 };
-/*
-int MMCQuantizer::vbox::partition(int start, int end, int pivot, int channel)
-{
-    //partition from Quicksort
-    uint8_t pivot_val = px[3*pivot+channel];
-    int i = start;
-    //move pivot to end
-    swap_px(pivot, end-1);
-    //scan along the range, swapping if value is less than pivot
-    for(int j = start; j < end; j++)
-    {
-        if(px[3*j+channel] < pivot_val)
-        {
-            swap_px(i,j);
-            i++;
-        }
-    }
-    //move the pivot to the middle
-    swap_px(i, end-1);
 
-    return i+1;
-};
-*/
-int MMCQuantizer::vbox::partition_by_value(int start, int end, float value, int channel)
+int MMCQuantizer::vbox::partition(int start, int end, float value, int channel, 
+                                  uint8_t& left_high, uint8_t right_low)
 {
-    std::cout << "\t\tMMCQuantizer::vbox::partition_by_value("<<start<<", "<<end<<", "<<value<<", "<<channel<<")"<<std::endl;
     int i = start, j;
-    //check if they're all equal
-    bool all_equal = true;
-    uint8_t v = px[3*start+channel];
+    left_high = min[channel];
+    right_low = max[channel];
+    uint8_t v, lowest = max[channel], highest = min[channel];
 
-
-    /**/
-    unsigned long hist[256];
-    for(int k = 0; k < 256; k++)
-        hist[k] = 0;
-    /**/
-
-
+    //quicksort esq, keep track of lowest and highest in each side
     for(j = start; j < end; j++)
     {
-        hist[int(px[3*j+channel])]++;
-        if(all_equal)
-            all_equal = (v == px[3*j+channel]);
-        if(px[3*j+channel] < value)
+        v = px[3*j+channel];
+        if(v < value)
         {
+            if(v > left_high) left_high = v;
             swap_px(i,j);
             i++;
         }
+        else
+        {
+            if(v < right_low) right_low = v;
+        }
+
+        if(v < lowest) lowest = v;
+        if(v > highest) highest = v;
     }
 
-    /**/
-    for(int k = 0; k < 256; k++)
-        if(hist[k] != 0)
-            std::cout << "\t\t\thist[" << k << "] = " << hist[k] << std::endl;
-    /**/
-
-    std::cout << "\t\t\tall_equal = " << all_equal << std::endl;
-    
-    if(all_equal)
+    //if all values are the same
+    if(lowest == highest)
         return -1;
-    set_front_back(i, end, channel);
-    set_front_back(start, i-1, channel);
+
     return i;
 };
 
-int MMCQuantizer::vbox::partition_channel(int ch)
+uint8_t MMCQuantizer::vbox::split_channel(int ch, uint8_t& value)
 {
-    std::cout << "\tMMCQuantizer::vbox::partition_channel(ch = " << ch << ")" << std::endl;
-    int first = 0, 
-        last = num_pixels-1, 
-        median = num_pixels / 2, 
-        pivot;
-    float l_bound = float(min[ch]),
-          u_bound = float(max[ch]);
+    int start = 0,
+        end   = num_pixels,
+        idx;
+    uint8_t low   = min[ch],
+            high  = max[ch],
+            lhigh = max[ch],
+            rlow  = min[ch],
+            last_partition;
 
-    while(last != first)
+    while(end-start > 1)
     {
-        std::cout << "bounds = (" << l_bound << ", " << u_bound << ")" << std::endl;
-        //partition around a value half way between the extreems
-        pivot = partition_by_value(first, last+1, (l_bound+u_bound)/2., ch);
-        std::cout << "\t\t-> " << pivot << std::endl;
+        //average then ceil
+        last_partition = int(0.5*(float(low)+float(high))+1.);
+        //partition on the mid value
+        idx = partition(start, end, float(last_partition),
+                        lhigh, rlow);
 
-        //if all the values we're left with are the same
-        if(pivot < 0)
+        //if all values between start & end are equal
+        if(idx < 0)
         {
-            //return either the pivot before or the pivot after the run of
-            //identical values
-            //choose the one furthest from its original position as the better
-            //median
-            if((first - 0) > (num_pixels-1 - last))
-                return first;
+            //Return the value which will get us closest to the median
+            if((start) >= (num_pixels - end))
+            {
+                //start is larger, put repeated values in rhs
+                return last_partition;
+            }
             else
-                return last+1;
+            {
+                //put repeated values in lhs
+                return last_partition + 1;
+            }
+
         }
 
-        //if the pivot is below the median, we only need to partition the upper part
-        if(pivot < median)
+        //if index is below middle
+        if(idx < num_pixels/2)
         {
-            l_bound = px[3*pivot+ch];
-            first = pivot;
+            //partition the rhs
+            start = idx;
+            low = rlow;
         }
-        //if the pivot is above the median, we only need to partition the lower part
-        else if(pivot > median)
+        //if index above middle
+        else if(idx > num_pixels/2)
         {
-            u_bound = px[3*(pivot-1)+ch];
-            last = pivot-1;
+            //partition lhs
+            end = idx;
+            high = lhigh;
         }
-        //special case where we happened to get the median
-        else if(pivot == median)
-            return pivot;
-
+        //idx == middle
+        else
+        {
+            start = idx;
+            end = idx+1;
+        }
     }
 
-    return first;
-};
-
-void MMCQuantizer::vbox::set_front_back(int start, int end, int channel)
-{    
-    uint8_t s = px[3*start+channel],
-            l  = px[3*start+channel];
-    int s_i = start,
-        l_i = start;
-    for(int j = start; j < end; j++)
-    {
-        if(px[3*j+channel] < s)
-        {
-            s = px[3*j+channel];
-            s_i = j;
-        }
-        else if(px[3*j+channel] > l)
-        {
-            l = px[3*j+channel];
-            l_i = j;
-        }
-    }
-    swap_px(start, s_i);
-    swap_px(end, l_i);
+    return last_partition;
 };
 
 void MMCQuantizer::vbox::split()
 {
-    std::cout << "MMCQuantizer::vbox::split()" << std::endl;
-    std::cout << "\tnum_pixels = " << num_pixels << std::endl;
-    if(num_pixels <= 0)
-        usleep(10000000);
     int ch = 0, i;
     uint8_t val = 0;
     //get largest dimension
     for(i = 0; i < 3; i++)
     {
-        std::cout << "\t(min["<< i << "], max[" << i<<"]) = (" << int(min[i]) << ", " << int(max[i]) << ")"<<std::endl;
         if(max[i] - min[i] > val)
         {
             val = max[i] - min[i];
             ch = i;
         }
     }
-    std::cout << "\tchannel = " << ch << std::endl;
 
-    //find the index of the median pixel in that channel
-    int split_index = partition_channel(ch);
-    std::cout << "\tsplit_index = " << split_index << std::endl;
-    int median = split_index;
-    split_channel = ch;
-    split_value = float(px[3*split_index+ch]);
+    //get split value
+    uint8_t split_value = get_split_value(ch),
+            lhigh, rlow;
+    int split_index;
 
     //if left is the largest distance
-    std::cout << "if(split_value - min[ch] > max[ch] - split_value)" << std::endl; 
-    std::cout << "if(" << split_value<<" - "<<int(min[ch])<<" > "<<int(max[ch])<<" - "<<split_value<<")" << std::endl; 
-    std::cout << "if("<<(split_value - min[ch])<<" > "<<(max[ch] - split_value)<<")" << std::endl; 
     if(split_value - min[ch] > max[ch] - split_value)
     {
         std::cout << "\tLHS split" << std::endl;
         //split half way along the left
         split_value = ((float)split_value+(float)min[ch])/2.;
-        split_index = partition_by_value(0, split_index, split_value, ch);
+        split_index = partition(0, split_index, split_value, ch, lhigh, rlow);
     }
     //if right is the largest distance
     else
@@ -446,14 +390,11 @@ void MMCQuantizer::vbox::split()
         std::cout << "\tRHS split" << std::endl;
         //split half way along the right
         split_value = ((float)split_value+(float)max[ch])/2.;
-        split_index = partition_by_value(split_index, num_pixels, split_value, ch);
+        split_index = partition(split_index, num_pixels, split_value, ch, lhigh, rlow);
     }
     
-    std::cout << "\tsplit_index = " << split_index << std::endl;
     left = new vbox(px, split_index);
     right = new vbox(px+3*split_index, num_pixels - split_index);
-    std::cout << "\t(left,right)->get_count() = (" << left->get_count() << ", " << right->get_count() << ")" << std::endl; 
-    std::cout << "MMCQuantizer::vbox::~split()" << std::endl;
 };
 
 
