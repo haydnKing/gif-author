22a6f8f9f403607c23997b28bcfa87a23700d50c
Haydn King <hjking734@gmail.com>
2016-06-26 12:26:16 +0100

    Begin simplifying factory

--- src/factory.h
+++ src/factory.h
@@ -2,33 +2,36 @@
 #define GIFAUTHOR_FACTORY_H
 
 #include <map>
+#include <string>
+
+#include "settings.h"
 
 using std::map;
 
 /**
  * A factory class template for Configureables
- *
- * The idea is that this will also help building command line options etc
+ * C: a Configureable
  */
-template<class E, class T> class Factory
+template<class C> class Factory
 {
     public:
-        static T *get(const E& type)
+
+        static C *get(const std::string& name)
         {
             return my_map.at(type);
         };
 
-        static bool register_type(const E& type, T* cfg)
+        static bool register_type(const std::string& name, C* cfg)
         {
-            auto r = my_map.insert(make_pair(type, cfg));
+            auto r = my_map.insert(make_pair(name, cfg));
             return r.second;
         };
 
     private:
-        static std::map<E, T*> my_map;
+        static std::map<std::string, C*> my_map;
 };
 
-template<class E, class T>
-std::map<E,T*> Factory<E,T>::my_map = std::map<E,T*>();
+template<class C>
+std::map<std::string,C*> Factory<std::string,C>::my_map = std::map<std::string,C*>();
 
 #endif // GIFAUTHOR_FACTORY_H
