ea5b14984d8ef89090ace7ff2400c20d5da95e74
Haydn King <hjking734@gmail.com>
2016-11-13 15:19:45 +0000

    process -> cline

---
+++ src/util/cline.cc
@@ -0,0 +1,2 @@
+#include "cline.h"
+
---
+++ src/util/cline.h
@@ -0,0 +1,15 @@
+#ifndef GIFAUTHOR_CLINE_H
+#define GIFAUTHOR_CLINE_H
+
+#include <string>
+#include <sstream>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <memory>
+
+
+
+
+#endif //GIFAUTHOR_CLINE_H
+
