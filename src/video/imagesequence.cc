45c236053cc2e9ea3313f06b57fb24a81a50a3cb
Haydn King <hjking734@gmail.com>
2017-03-26 19:01:33 +0100

    Basic image loading and preprocessing

--- src/video/imagesequence.cc
+++ src/video/imagesequence.cc
@@ -3,17 +3,36 @@
 Frame::Frame(const std::string& filename, int delay):
     delay(delay)
 {
-    cv::Mat image;
-    image = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
+    cv::Mat image = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
     if(!image.data) {
         throw "No image data loaded from \"" + filename + "\"";
     }
-    cv::cvtColor(image, &this, CV_BGR2RGBA);
+    cv::cvtColor(image, my_mat, CV_BGR2RGBA);
 };
+        
+Frame::Frame(int width, int height, int delay):
+    my_mat(height, width, CV_8UC4),
+    delay(delay)
+{};
 
-pFrame Frame::from_file(const std::string& filename, int delay)
+Frame::Frame(const cv::Mat& mat, int delay):
+    my_mat(mat),
+    delay(delay)
+{};
+
+pFrame Frame::create_from_file(const std::string& filename, int delay)
 {
     return pFrame(new Frame(filename, delay));
+};
+
+pFrame Frame::create(int width, int height, int delay)
+{
+    return pFrame(new Frame(width, height, delay));
+};
+
+pFrame Frame::create(const cv::Mat& mat, int delay)
+{
+    return pFrame(new Frame(mat, delay));
 };
 
 int Frame::get_delay() const
@@ -25,12 +44,24 @@ void Frame::set_delay(int d)
 {
     delay = d;
 };
+        
+pFrame Frame::resize(int width, int height, int interpolation) const
+{
+    pFrame out = Frame::create(width, height, get_delay());
+    cv::resize(mat(), out->mat(), out->size(), 0, 0, interpolation);
+    return out;
+}
 
-Sequence::Sequence(const std::vector<pFrame>& frames)
+pFrame Frame::crop(int x, int y, int width, int height) const
+{
+    return Frame::create(mat()(cv::Rect(x,y,width,height)), get_delay());
+};
+
+Sequence::Sequence(const std::vector<pFrame>& frames):
     std::vector<pFrame>(frames)
 {};
 
-pSequence Sequence::from_filenames(const std::vector<std::string>& fnames, int delay)
+pSequence Sequence::create_from_filenames(const std::vector<std::string>& fnames, int delay)
 {
     pS = pSequence(new Sequence());
     for(auto f: fnames)
@@ -40,7 +71,7 @@ pSequence Sequence::from_filenames(const std::vector<std::string>& fnames, int d
     return pS;
 };
 
-pSequence Sequence::from_filenames(const std::vector<std::string>& fnames, const vector<int>& delays)
+pSequence Sequence::create_from_filenames(const std::vector<std::string>& fnames, const vector<int>& delays)
 {
     pS = pSequence(new Sequence());
     for(int i = 0; i < fnames.size(); i++)
@@ -50,7 +81,33 @@ pSequence Sequence::from_filenames(const std::vector<std::string>& fnames, const
     return pS;
 };
 
-pSequence Sequence::from_frames(const std::vector<pFrame>& frames)
+pSequence Sequence::create_from_frames(const std::vector<pFrame>& frames)
 {
     return pSequence(new Sequence(frames));
-}
+};
+
+pSequence Sequence::create()
+{
+    return pSequence(new Sequence());
+};
+        
+pSequence Sequence::resize(int width, int height) const
+{
+    pSequence r = Sequence::create();
+    for(auto it = begin(); it < end(); it++)
+    {
+        r->push_back((*it)->resize(width, height));
+    }
+    return r;
+};
+        
+pSequence Sequence::crop(int x, int y, int width, int height) const
+{
+    pSequence r = Sequence::create();
+    for(auto it = begin(); it < end(); it++)
+    {
+        r->push_back((*it)->crop(x, y, width, height));
+    }
+    return r;
+};
+
--- src/video/imagesequence.h
+++ src/video/imagesequence.h
@@ -10,21 +10,31 @@
 class Frame;
 typedef std::shared_ptr<Frame> pFrame;
 
-class Frame : public cv::Mat
+class Frame 
 {
     public:
         virtual ~Frame() {};
 
-        static pFrame from_file(const std::string& filename, int delay=0);
+        static pFrame create(int width, int height, int delay=0);
+        static pFrame create(const cv::Mat& mat, int delay=0);
+        static pFrame create_from_file(const std::string& filename, int delay=0);
 
         int get_delay() const;
         void set_delay(int d);
 
+        const cv::Mat mat() const {return my_mat;}; 
+
+        pFrame resize(int width, int height, int interpolation=cv::INTER_AREA) const;
+        pFrame crop(int x, int y, int width, int height) const;
+
     private:
         Frame(const std::string& filename, int delay);
+        Frame(int width, int height, int delay);
+        Frame(const cv::Mat& mat, int delay);
 
     protected:
         int delay;
+        cv::Mat my_mat;
 };
 
 
@@ -40,9 +50,20 @@ class Sequence : public std::vector<pFrame>
         /**
          * Initiate the image sequence from files
          */
-        static pSequence from_filenames(const std::vector<std::string>& fnames, int delay);
-        static pSequence from_filenames(const std::vector<std::string>& fnames, const vector<int>& delay);
-        static pSequence from_frames(const std::vector<pFrame>& frames);
+        static pSequence create_from_filenames(const std::vector<std::string>& fnames, int delay);
+        static pSequence create_from_filenames(const std::vector<std::string>& fnames, 
+                                               const vector<int>& delay);
+        static pSequence create_from_frames(const std::vector<pFrame>& frames);
+        static pSequence create();
+
+        /*
+         * batch resize
+         */
+        pSequence resize(int width, int height) const;
+        /*
+         * batch crop
+         */
+        pSequence crop(int x, int y, int width, int height) const;
         
     private:
         Sequence(const std::vector<pFrame>& frames);
